49cf301b4d53ed07f7769f1558f882468b216ee5
Elodias2001 <olouwagnon@gmail.com>
2024-06-24 16:08:05 +0200

    6Commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,69 @@
 <template>
-  <div>
-    <!-- <AdminAppSideBar>
-      <AdminNavbar />
-    </AdminAppSideBar> -->
-    <!-- <slot /> -->
-    <AdminAppSideBar>
-      <template #navbar>
-        <AdminNavbar />
-      </template>
-      <template v-slot>
-        <!-- Dynamic content goes here -->
-        <div>
-          <h1>Page Title</h1>
-          <p>This is some dynamic content.</p>
-        </div>
-      </template>
-    </AdminAppSideBar>
+  <div class="container grid grid-cols-1 lg:grid-cols-[300px_minmax(0,1fr)] lg:gap-10">
+    <Sidebar class="border-r" />
+    <main class="mx-auto w-full pb-5 md:max-w-2xl lg:max-w-full">
+      <div
+        class="sticky top-0 mb-10 flex w-full items-center justify-between border-b bg-background pb-7 pt-5"
+        ref="headerRef"
+        :style="styles"
+      >
+        <h1 class="text-3xl font-bold tracking-tight">{{ $t("appearance") }}</h1>
+        <LanguageSwitcher />
+      </div>
+
+      <slot />
+    </main>
   </div>
+  <UiToastToaster />
+  <UiVueSonner />
 </template>
 
-<script setup></script>
+<script setup>
+  import { useOnline } from "~/utils/useOnline";
+  import { useFixedHeader } from "vue-use-fixed-header";
+
+  const headerRef = ref(null);
+  const { styles } = useFixedHeader(headerRef);
+
+  const title = ref("ADMIN DASHBOARD");
+  const description = ref("Consulter le tableau de board de notre application après connexion.");
+  const img = "/cover.png";
+
+  useSeoMeta({
+    title: title.value,
+    titleTemplate: "%s | DATTEBAYO",
+    description: description.value,
+    ogTitle: title.value,
+    ogDescription: description.value,
+    ogImage: img,
+    twitterTitle: title.value,
+    twitterDescription: description.value,
+    twitterImage: img,
+    twitterCard: "summary_large_image",
+  });
+
+  useHead({
+    htmlAttrs: {
+      lang: "en",
+    },
+    link: [
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "/icon.svg",
+      },
+    ],
+  });
+  // Watch for changes in online status and show notification
+  watch(isOnline, (online) => {
+    if (!online) {
+      push.error({
+        title: "Connexion",
+        message: "Vous n'êtes pas connecté à Internet.",
+      });
+    }
+  });
+  const save = () => {
+    useSonner.success("Saved", { description: "Settings saved successfully" });
+  };
+</script>
